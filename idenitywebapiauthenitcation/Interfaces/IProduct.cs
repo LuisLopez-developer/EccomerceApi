c0b78af7d4e4f851e5f8cbac815c519ff41795f9
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-25 00:42:12 -0500

    se ñadio la inteface y los services de producto

---
+++ idenitywebapiauthenitcation/Interfaces/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using EccomerceApi.Entity;
+
+namespace EccomerceApi.Interfaces
+{
+    public interface IProduct
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product product);
+        Task<bool> UpdateAsync(int id, Product product);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ idenitywebapiauthenitcation/Services/ProductService.cs
@@ -0,0 +1,71 @@
+﻿using EccomerceApi.Data;
+using EccomerceApi.Entity;
+using EccomerceApi.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EccomerceApi.Services
+{
+    public class ProductService : IProduct
+    {
+        private readonly IdentityDbContext _identityDbContext;
+
+        public ProductService(IdentityDbContext identityDbContext)
+        {
+            _identityDbContext = identityDbContext;
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            var productList = await _identityDbContext.Products.ToListAsync();
+            return productList;
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _identityDbContext.Products.Where(f => f.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            await _identityDbContext.AddAsync(product);
+            await _identityDbContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> UpdateAsync(int id, Product product)
+        {
+            var existingProduct = await _identityDbContext.Products.Where(f => f.Id == id).FirstOrDefaultAsync();
+
+            if (existingProduct != null)
+            {
+                existingProduct.Name = product.Name;
+                existingProduct.Code = product.Code;
+                existingProduct.IdState = product.IdState;
+                existingProduct.Date = product.Date;
+                existingProduct.Price = product.Price;
+                existingProduct.Cost = product.Cost;
+                existingProduct.Existence = product.Existence;
+                existingProduct.ProductBrandId = product.ProductBrandId;
+                existingProduct.IdProductCategory = product.IdProductCategory;
+            }
+
+            await _identityDbContext.SaveChangesAsync();
+            return existingProduct?.Id > 0;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _identityDbContext.Products.Where(f => f.Id == id).FirstOrDefaultAsync();
+
+            if(product != null)
+            {
+                _identityDbContext.Products.Remove(product);
+                await _identityDbContext.SaveChangesAsync();
+                id = product.Id;
+            }
+
+            return product?.Id > 0;
+        }
+
+    }
+}
