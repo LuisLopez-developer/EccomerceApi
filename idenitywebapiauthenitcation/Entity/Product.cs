3f7bfc99f16d0912e00d6a0aee3317d9c1b14015
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-30 15:02:35 -0500

    cambios a requerido

--- idenitywebapiauthenitcation/Entity/Product.cs
+++ idenitywebapiauthenitcation/Entity/Product.cs
@@ -6,13 +6,13 @@ namespace EccomerceApi.Entity
     public partial class Product
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Code { get; set; } //SKU
+        public required string Name { get; set; }
+        public required string Code { get; set; } //SKU
         public DateTime? Date { get; set; }
         public DateTime UpdateAt { get; set; }
-        public decimal? Price { get; set; }
+        public required decimal Price { get; set; }
         public decimal? Cost { get; set; }
-        public int? Existence { get; set; }
+        public required int Existence { get; set; }
         public bool IsVisible { get; set; } = false;
         public string? Description { get; set; }
         public string BarCode { get; set; } = "sn";
@@ -20,10 +20,10 @@ namespace EccomerceApi.Entity
         public int? StateId { get; set; }
         public virtual State State { get; set; }
 
-        public int? ProductBrandId { get; set; }
+        public required int ProductBrandId { get; set; }
         public virtual ProductBrand ProductBrand { get; set; }
 
-        public int? ProductCategoryId { get; set; }
+        public required int ProductCategoryId { get; set; }
         public virtual ProductCategory ProductCategory { get; set; }
 
         public virtual ICollection<EntryDetail> EntryDetails { get; set; } = new List<EntryDetail>();
