3c43ed494b7a7fc93765a18d91e6763e0b041529
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-30 14:58:27 -0500

    se  añadio campos requeridos para el modelo de crear un producto

--- idenitywebapiauthenitcation/Model/ProductModel/CreateModel/ProductCreateModel.cs
+++ idenitywebapiauthenitcation/Model/ProductModel/CreateModel/ProductCreateModel.cs
@@ -6,15 +6,15 @@ namespace EccomerceApi.Model.ProductModel.CreateModel
     public class ProductCreateModel
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Code { get; set; }
+        public required string Name { get; set; }
+        public required string Code { get; set; }
         public int? StateId { get; set; }
         public DateTime? Date { get; set; }
-        public decimal? Price { get; set; }
-        public decimal? Cost { get; set; }
-        public int? Existence { get; set; }
-        public int? ProductBrandId { get; set; }
-        public int? ProductCategoryId { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal? Cost { get; set; }
+        public required int Existence { get; set; }
+        public required int ProductBrandId { get; set; }
+        public required int ProductCategoryId { get; set; }
 
         public List<ProductPhotoViewModel>? Photos { get; set; }
         public ProductSpecificationViewModel? Specifications { get; set; }
