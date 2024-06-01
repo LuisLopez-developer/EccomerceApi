ca73d96bea3889e9c83c66219527bbed4d55c8bc
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-06-01 11:14:08 -0500

    se añadio un helper pra obtener la hora de peru

---
+++ idenitywebapiauthenitcation/Herlpers/getTimePeruHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace EccomerceApi.Herlpers
+{
+    public static class getTimePeruHelper
+    {
+        public static DateTime GetCurrentTimeInPeru()
+        {
+            var peruTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");
+            var currentTimePeru = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, peruTimeZone);
+            return currentTimePeru;
+        }
+    }
+}
