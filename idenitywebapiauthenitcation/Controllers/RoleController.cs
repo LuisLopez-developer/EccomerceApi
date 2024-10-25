8cb75ef342f394f487c3c0d462ce92ad71b2c921
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-25 11:24:24 -0500

    Eliminar importación innecesaria en RoleController.cs
    Se ha eliminado la importación de `Microsoft.AspNetCore.Http` en el archivo `RoleController.cs`. Esto indica que las funcionalidades proporcionadas por este espacio de nombres ya no son necesarias en dicho archivo.

--- idenitywebapiauthenitcation/Controllers/RoleController.cs
+++ idenitywebapiauthenitcation/Controllers/RoleController.cs
@@ -1,7 +1,6 @@
 ﻿using EccomerceApi.Interfaces;
 using EccomerceApi.Model;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
