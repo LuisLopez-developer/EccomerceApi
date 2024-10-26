e9c57ef834354a3069ca9b7c911ae40ff786ff31
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-25 20:19:30 -0500

    Refactor y mejora lógica de gestión de carritos
    Se ha cambiado la interfaz del repositorio de `IRepository<Cart>` a `ICartRepository` en la clase `AddCartUseCase`.
    
    Se ha añadido lógica para verificar si el usuario ya tiene un carrito. Si el usuario ya tiene un carrito, se actualiza el carrito existente en lugar de crear uno nuevo.
    
    Si el carrito contiene un producto que ya está en el carrito existente, se actualiza la cantidad del producto en lugar de añadir un nuevo ítem.
    
    Si el usuario no tiene un carrito, se crea uno nuevo como antes.

--- AplicationLayer/Sale/AddCartUseCase.cs
+++ AplicationLayer/Sale/AddCartUseCase.cs
@@ -5,10 +5,10 @@ namespace AplicationLayer.Sale
 {
     public class AddCartUseCase<TDTO>
     {
-        private readonly IRepository<Cart> _cartRepository;
         private readonly IMapper<TDTO, Cart> _mapper;
+        private readonly ICartRepository _cartRepository;
 
-        public AddCartUseCase(IRepository<Cart> cartRepository, IMapper<TDTO, Cart> mapper)
+        public AddCartUseCase(ICartRepository cartRepository, IMapper<TDTO, Cart> mapper)
         {
             _cartRepository = cartRepository;
             _mapper = mapper;
@@ -20,6 +20,31 @@ namespace AplicationLayer.Sale
 
             if (cart.CartItems.Count <= 0)
                 throw new ValidationException("El carrito debe conterner Items.");
+
+            // verificar si el usuario ya tiene un carrito, si ya tiene solo actualizas, y si
+            // no tiene, creas uno nuevo. Pero si el item contiene un producto que ya esta en el carrito
+            // solo actualizas la cantidad del producto.
+            if (await _cartRepository.UserHasCartAsync(cart.UserId))
+            {
+                var existingCart = await _cartRepository.GetByUserIdAsync(cart.UserId);
+                foreach (var item in cart.CartItems)
+                {
+                    var existingItem = existingCart.CartItems.FirstOrDefault(x => x.ProductId == item.ProductId);
+                    if (existingItem != null)
+                    {
+                        // Crear un nuevo CartItem con la cantidad actualizada
+                        var updatedItem = new CartItem(existingItem.Id, existingItem.ProductId, existingItem.Quantity + item.Quantity, existingItem.CreatedAt);
+                        existingCart.CartItems.Remove(existingItem);
+                        existingCart.CartItems.Add(updatedItem);
+                    }
+                    else
+                    {
+                        existingCart.CartItems.Add(item);
+                    }
+                }
+                await _cartRepository.UpdateAsync(existingCart.Id, existingCart);
+                return;
+            }
 
             await _cartRepository.AddAsync(cart);
         }
