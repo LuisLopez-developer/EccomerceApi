f161917d35132c140d75b759e22251a69a487122
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-25 20:04:11 -0500

    Añadir GetTotalProductQuantityByUserIdUseCase
    Se ha añadido un nuevo espacio de nombres `AplicationLayer.Sale`.
    Se ha creado la clase `GetTotalProductQuantityByUserIdUseCase` con un
    campo privado `_cartRepository` de tipo `ICartRepository` y un
    constructor que lo inicializa. También se ha añadido el método
    `ExecuteAsync` que toma un `userId` y devuelve la cantidad total de
    productos para ese usuario llamando al método
    `GetTotalProductQuantityByUserIdAsync` del repositorio `_cartRepository`.

---
+++ AplicationLayer/Sale/GetTotalProductQuantityByUserIdUseCase.cs
@@ -0,0 +1,17 @@
+﻿namespace AplicationLayer.Sale
+{
+    public class GetTotalProductQuantityByUserIdUseCase
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public GetTotalProductQuantityByUserIdUseCase(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public async Task<int> ExecuteAsync(string userId)
+        {
+            return await _cartRepository.GetTotalProductQuantityByUserIdAsync(userId);
+        }
+    }
+}
