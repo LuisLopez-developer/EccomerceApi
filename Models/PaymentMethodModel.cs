31a3b817e7010e0578af925ab19f49de0dfb8f02
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-26 22:00:50 -0500

    Cambio de ICollection a List en Orders de PaymentMethodModel
    Se ha modificado la propiedad `Orders` en la clase `PaymentMethodModel`.
    Anteriormente, esta propiedad era de tipo `ICollection<OrderModel>` y se
    inicializaba con una colección vacía (`[]`). Ahora, la propiedad es de
    tipo `List<OrderModel>` y se inicializa con una nueva instancia de
    `List<OrderModel>`.

--- Models/PaymentMethodModel.cs
+++ Models/PaymentMethodModel.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public virtual ICollection<OrderModel> Orders { get; set; } = [];
+        public virtual List<OrderModel> Orders { get; set; } = new List<OrderModel>();
     }
 }
